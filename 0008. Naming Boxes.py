2812e47b6df977ddce1594950eb3e2abfb743552
Unnati Samaiyar <104674490+UnnatiSamaiyar@users.noreply.github.com>
2023-04-03 14:47:15 +0530

    Create 0008. Naming Boxes.py

---
+++ 0008. Naming Boxes.py
@@ -0,0 +1,9 @@
+length = 11 
+width = 13 
+print(length * width)
+print(2*(length + width))
+
+#replace the _ with digits. Update the width from 13 to 15
+width = 15 
+print(length * width)
+print(2*(length + width))
