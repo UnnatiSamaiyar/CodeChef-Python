122c32959c3081a744edd1ef2366a25974d91249
Unnati Samaiyar <104674490+UnnatiSamaiyar@users.noreply.github.com>
2023-04-04 17:32:06 +0530

    Create 0027 - Modifying Strings.py

---
+++ 0027 - Modifying Strings.py
@@ -0,0 +1,7 @@
+a = "Bootstrap"
+b = "TYPESCRIPT"
+print( a.upper()) #print the string a in upper case
+print( b.lower() ) #print the string b in lower case
+c = "Welcome to ChefCode! You can learn Python with ChefCode"
+d = c.replace("ChefCode", "CodeChef")
+print(d)
